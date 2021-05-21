915f2d7aa493b0ca213d5882435878ff2cd4c7aa
unknown <ynarula678@gmail.com>
2021-05-21 16:12:35 +0530

    nav

--- lib/screens/feed.dart
+++ lib/screens/feed.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class FeedPage extends StatefulWidget {
   @override
@@ -24,6 +26,7 @@ class _FeedPageState extends State<FeedPage> {
               ))
         ],
       ),
+      backgroundColor: HexColor('#efd3d7'),
       body: Container(
         padding: EdgeInsets.all(5.0),
         child: Column(
@@ -36,6 +39,28 @@ class _FeedPageState extends State<FeedPage> {
           ],
         ),
       ),
+      bottomNavigationBar: CurvedNavigationBar(
+        color : HexColor('#feeafa'),
+        backgroundColor: HexColor('#efd3d7'),
+        items: <Widget>[
+          Icon(Icons.home_outlined ,size: 15, color: Colors.black),
+          Icon(Icons.add, size: 15, color: Colors.black),
+          Icon(Icons.perm_identity, size: 15, color: Colors.black)
+        ],
+        animationDuration:Duration(
+          milliseconds: 450,
+        ),
+        height: 50.0,
+        animationCurve: Curves.easeInOut,
+        onTap: (index){
+          debugPrint('Current index is $index');
+
+        },
+        buttonBackgroundColor: HexColor('#feeafa'),
+
+      ),
+
+      
     );
   }
 }
